21e4165a9a30eb86647c3dcf997ad19cc14fed8e
mattericbrown <matt.eric.brown@gmail.com>
2016-07-10 22:49:22 -0700

    updated functions to allow for less than three players at a time. Added reset input and styling.  added a feature that declares a winner. updated README file

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# RacecaR
-Browser racing game
+# Browser Racing Game
+A simple browser based racing game for two or more players. The goal of which is to get your player across the board first.
+
+#Technologies Used
+-HTML
+-CSS
+-JavaScript
+-jQuery
+
+#Existing Features
+-Character Selection
+-Avatar display
+-Winner announcement
+-Custom styling
+
+#Planned Features
+-Countdown Timer
+-Score Tracking
+-Better accuracy for determining winner
--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 $(function(){
-  console.log("hey");
   $('.hillary').one('click', function(){
     $('.playerOne').append('<img id="clinton"src="http://i.imgur.com/QCT8uLb.png"></img>');
   });
@@ -8,54 +7,47 @@ $(function(){
   });
   $('.bernie').one('click', function(){
     $('.playerThree').append('<img id="bernie"src="http://i.imgur.com/pWvBDKC.png"></img>');
-  });
+});
 
   $(document).keypress(function(event){
-    if($('#clinton').css('margin-left') <= '90' +'%'){
-      if(event.which==113){
+      if(event.which==13){
         $('#clinton').animate({
           marginLeft: "+=" + createNum() +"%"
         });
         $('#clinton').clearQueue();
+      if($('#clinton').css('margin-left') <= '90' +'%'){
+      }else {
+        $('.hillarywins').append('<img src="http://i.imgur.com/VPeKkas.png"></img>');
       }
-    }else{
-      alert("hillary has won");
-    }
-  });
-  $(document).keypress(function(event){
-    if($('#trump').css('margin-left') <='90' +'%'){
-      if(event.which==107){
+      }
+    });
+  $(document).keyup(function(event){
+      if(event.which==16){
         $('#trump').animate({
           marginLeft: "+=" + createNum() +"%"
         });
         $('#trump').clearQueue();
+      if($('#trump').css('margin-left') <= '90' +'%'){
+      }else{
+        $('.trumpwins').append('<img src="http://i.imgur.com/RcOfI3H.png"></img>');
       }
-    }else{
-      alert("trum has won");
-    }
+      }
   });
-  $(document).keypress(function(event){
-    if($('#bernie').css('margin-left') <= '90'+'%'){
-      if(event.which==98){
+  $(document).keydown(function(event){
+      if(event.which==32){
         $('#bernie').animate({
           marginLeft: '+=10%'
         });
         $('#bernie').clearQueue();
+      if($('#bernie').css('margin-left') <= '90'+'%'){
+      } else{
+        $('.berniewins').append('<img src="http://i.imgur.com/3FxN2Vw.png"></img>');
       }
-    }else{
-      alert("bernie has won");
-    }
-  });
+      }
+    });
 });
 function createNum(){
 var nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 var randomNum = nums[Math.floor(Math.random() * nums.length)];
 return randomNum;
 }
-
-
-
-//var keycode = (event.keyCode ? event.keyCode : event.which);
- //if(keycode == '13'){
-     //alert('You pressed a "enter" key in somewhere');
- //}
