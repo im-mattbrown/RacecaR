fd712bdac3f98700923286b2d4115cb84e68895d
mattericbrown <matt.eric.brown@gmail.com>
2016-07-11 15:05:51 -0700

    Hid footer on page load unitl ready button clicked made reset button, updated win condition to end the race completely, applied styling to buttons

--- main.js
+++ main.js
@@ -1,50 +1,53 @@
 $(function(){
+  $('footer').hide();
+  $('.ready').on('click', function(){
+    $('footer').show();
+  });
   $('.hillary').one('click', function(){
-    $('.playerOne').append('<img id="clinton"src="http://i.imgur.com/QCT8uLb.png"></img>');
+    $('.playerOne').append('<img id="clinton"src="http://i.imgur.com/QCT8uLb.png">');
   });
   $('.donald').one('click', function(){
-    $('.playerTwo').append('<img id="trump"src="http://i.imgur.com/tsUDnqG.png"></img>');
+    $('.playerTwo').append('<img id="trump"src="http://i.imgur.com/tsUDnqG.png">');
   });
   $('.bernie').one('click', function(){
-    $('.playerThree').append('<img id="bernie"src="http://i.imgur.com/pWvBDKC.png"></img>');
-});
-
-  $(document).keypress(function(event){
-      if(event.which==13){
-        $('#clinton').animate({
-          marginLeft: "+=" + createNum() +"%"
-        });
-        $('#clinton').clearQueue();
-      if($('#clinton').css('margin-left') <= '90' +'%'){
-      }else {
-        $('.hillarywins').append('<img src="http://i.imgur.com/VPeKkas.png"></img>');
-      }
-      }
-    });
-  $(document).keyup(function(event){
-      if(event.which==16){
-        $('#trump').animate({
-          marginLeft: "+=" + createNum() +"%"
-        });
-        $('#trump').clearQueue();
-      if($('#trump').css('margin-left') <= '90' +'%'){
-      }else{
-        $('.trumpwins').append('<img src="http://i.imgur.com/RcOfI3H.png"></img>');
-      }
-      }
+    $('.playerThree').append('<img id="bernie"src="http://i.imgur.com/pWvBDKC.png">');
   });
-  $(document).keydown(function(event){
+
+  $(document).keyup(function(event){
+      if(event.which==13){
+        $('#clinton').clearQueue().animate({
+          marginLeft: "+=" + createNum() +"%"
+        });
+      if($('#clinton').css('marginLeft') <= '95' +'%'){
+      }else if($('#clinton').css('marginLeft') >= '95' +'%'){
+        $('.hillarywins').append('<img src="http://i.imgur.com/VPeKkas.png">');
+          $('#clinton').off(event);
+       }
+      }
+      if(event.which==16){
+        $('#trump').clearQueue().animate({
+          marginLeft: "+=" + createNum() +"%"
+        });
+      if($('#trump').css('marginLeft') <= '95' +'%'){
+      }else if($('#trump').css('marginLeft') >= '95' +'%'){
+        $('.trumpwins').append('<img src="http://i.imgur.com/1WhnSiJ.png">');
+          $('#trump').off(event);
+       }
+      }
       if(event.which==32){
-        $('#bernie').animate({
+        $('#bernie').clearQueue().animate({
           marginLeft: '+=10%'
         });
-        $('#bernie').clearQueue();
-      if($('#bernie').css('margin-left') <= '90'+'%'){
-      } else{
-        $('.berniewins').append('<img src="http://i.imgur.com/3FxN2Vw.png"></img>');
+      if($('#bernie').css('marginLeft') <= '95'+'%'){
+      }else if($('#bernie').css('marginLeft') >= '95'+'%'){
+        $('.berniewins').append('<img src="http://i.imgur.com/3FxN2Vw.png">');
+          $('#bernie').off(event);
+       }
       }
-      }
-    });
+  });
+  $('.reset').click(function(){
+    location.reload();
+  });
 });
 function createNum(){
 var nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
